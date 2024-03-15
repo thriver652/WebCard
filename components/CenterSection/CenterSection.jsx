fb408e4f1ab047e5eb5e24f262e8a4368e317b60
thriver652 <tvspraneeth6502@gmail.com>
2024-03-15 16:06:50 +0530

    THrid Push

--- components/CenterSection/CenterSection.jsx
+++ components/CenterSection/CenterSection.jsx
@@ -1,24 +1,18 @@
 import React from "react";
-import "./CenterSection.scss";
+import "./CenterSection.scss"; // Import your SCSS file here
 import TransHist from "./TransHist/TransHist";
 const CenterSection = () => {
   return (
-    // <div className="center-sec">
-    //   <div className="card">Cards</div>
-    //   <div className="card"></div>
-    //   <div className="card">
-    //     <TransHist />
-    //   </div>
-    //   <div className="card"></div>
-    // </div>
-    <div style={{ flexDirection: "row", display: "flex" }}>
-      <div>
-        <div className="card">Hello</div>
-        <div className="card">Hello</div>
+    <div className="center-section">
+      <div className="row">
+        <div className="card">Card 1</div>
+        <div className="card">Card 2</div>
       </div>
-      <div>
-        <div className="card">Hello</div>
-        <div className="card">Hello</div>
+      <div className="row">
+        <div className="card">
+          <TransHist />
+        </div>
+        <div className="card">Card 4</div>
       </div>
     </div>
   );
